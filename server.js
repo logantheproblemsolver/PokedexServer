44a3b358f634fd797e778f8053ded9650984d00d
Logan Ellis <mrloganellis@gmail.com>
2020-02-11 22:08:05 -0500

    code is ready for deployment

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const POKEDEX = require('./pokedex.json')
 
 const app = express();
 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
 app.use(helmet())
-app.use(morgan('dev'))
 app.use(cors())
 
 
@@ -60,10 +62,19 @@ function handleGetPokemon(req, res) {
 app.get('/pokemon', handleGetPokemon)
 
 
-const PORT = 8000
-
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+    let response 
+    if (process.env.NODE_ENV === 'production') {
+        response = {error: {message: 'server error'}}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
 })
+
+
+const PORT = process.env.PORT || 8000
+
+
+app.listen(PORT)
 
